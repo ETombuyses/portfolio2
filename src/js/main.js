f143af7174942864dd050cf90f929f475db33d18
ETombuyses <e.tombuyses@gmail.com>
2019-03-20 14:20:31 +0100

    animated background

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,69 @@
+/* Full background with squares */
+
+const background = document.getElementById('background');
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+const numberOfSquaresPerLine = windowWidth / 65;
+const numberOfLines = windowHeight / 65;
+
+//width of square in CSS
+const width = 60;
+let x = 15;
+let y = 15;
+
+function createSquare(x, y) {
+  let square = document.createElement('div');
+  square.classList.add('square');
+  square.style.left = x + 'px';
+  square.style.top = y + 'px';
+  window.addEventListener('resize', () => {
+    square.remove();
+  })
+  background.appendChild(square);
+}
+
+function squares() {
+  if (window.innerWidth > 1099 && window.innerHeight > 600) {
+    for (let j = 1; j <= numberOfLines; j++) {
+      for (let i = 1; i <= numberOfSquaresPerLine; i++) {
+        createSquare(x, y);
+        x += 20 + width;
+      }
+      x = 15;
+      y += 20 + width;
+    }
+  }
+}
+
+squares();
+
+window.addEventListener('resize', () => {
+  x = 15;
+  y = 15;
+  setTimeout(() => {
+    squares();
+  }, 100)
+})
+
+
 /* burger menu */
 
-var nav = document.getElementById('navigation');
-var burgerIcon = document.getElementById('burgerIcon');
-var pageLinks = document.getElementById('pageLinks');
-var externalLinks = document.getElementById('externalLinks');
+const nav = document.getElementById('navigation');
+const burgerIcon = document.getElementById('burgerIcon');
+const pageLinks = document.getElementById('pageLinks');
+const externalLinks = document.getElementById('externalLinks');
 
 nav.addEventListener('click', (event) => {
   event.stopPropagation();
-  if (nav.classList.contains('is-open')) {     
-     pageLinks.classList.remove('is-open');
-     externalLinks.classList.remove('is-open');
+  if (nav.classList.contains('is-open')) {
+    pageLinks.classList.remove('is-open');
+    externalLinks.classList.remove('is-open');
 
-     setTimeout(() =>{
+    setTimeout(() => {
       nav.classList.remove('is-open');
       burgerIcon.classList.remove('is-open');
 
-     }, 200);
+    }, 200);
   } else {
     nav.classList.add('is-open');
     burgerIcon.classList.add('is-open');
@@ -23,54 +71,55 @@ nav.addEventListener('click', (event) => {
     setTimeout(() => {
       pageLinks.classList.add('is-open');
       externalLinks.classList.add('is-open');
-     }, 400);
+    }, 400);
   }
 });
 
 
 document.addEventListener('click', () => {
-  
+
   if (nav.classList.contains('is-open')) {
     pageLinks.classList.remove('is-open');
-     externalLinks.classList.remove('is-open');
-
-     setTimeout(() =>{
+    externalLinks.classList.remove('is-open');
+    setTimeout(() => {
       nav.classList.remove('is-open');
       burgerIcon.classList.remove('is-open');
-
-     }, 200);
+    }, 200);
   }
 })
 
 //prevent clicking on nav when clicking on an external link
+const links = document.querySelectorAll('#navLink');
 
-var links = document.querySelectorAll('#navLink');
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.stopPropagation();
+  })
+})
 
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', (event) => {
-  event.stopPropagation(); 
-  });
-}
 
 
 
 /* Flip the card on click */
-var flipCard = document.getElementById('flipCard');
 
-flipCard.addEventListener('click', function() {
+const flipCard = document.getElementById('flipCard');
+
+flipCard.addEventListener('click', () => {
   flipCard.classList.toggle('is-flipped');
 })
 
 
+
+
 /* Show content once the card has been flipped */
 
-var backCardTitle = document.getElementById('backCardTitle');
-var backCard = document.getElementById('backCard');
-var backCardText = document.getElementById('backCardText');
-var backSide = false;
+const backCardTitle = document.getElementById('backCardTitle');
+const backCard = document.getElementById('backCard');
+const backCardText = document.getElementById('backCardText');
+let backSide = false;
 
-flipCard.addEventListener('click', function() {
+flipCard.addEventListener('click', () => {
   backSide = !backSide;
 
   if (window.innerWidth < 1200) {
@@ -78,20 +127,18 @@ flipCard.addEventListener('click', function() {
     backCard.classList.toggle('is-flipped');
     backCardText.classList.toggle('is-visible');
   }
- 
+
   //if we resize the window when the card is flipped
-  window.addEventListener('resize' ,function() {
+  window.addEventListener('resize', () => {
     if (window.innerWidth > 1099 && window.innerHeight > 600) {
       backCardTitle.classList.remove('is-visible');
       backCard.classList.remove('is-flipped');
       backCardText.classList.remove('is-visible');
-    } 
+    }
     else if (backSide) {
       backCardTitle.classList.add('is-visible');
       backCard.classList.add('is-flipped');
       backCardText.classList.add('is-visible');
     }
   })
-
-
 });
